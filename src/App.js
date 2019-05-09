b59b42ece9552cafab564f97670133a09c2cd886
Lahiru Jayamanna <lsampath999@gmail.com>
2019-05-09 07:36:28 +0530

    Add redux support to maintain state.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
-import {Hello} from "./Hello";
+import {Hello} from './Hello';
 import logo from './images/react-redux.svg';
 
-class App extends Component {
-	state = {
-		tech: 'Redux'
-	};
+import {createStore} from 'redux';
+import {reducer} from './reducers';
 
+const initialState = { tech: 'Redux'};
+const store = createStore(reducer, initialState);
+
+class App extends Component {
 	render() {
 		return (
 			<div className="app">
 				<header className="app-header">
 					<img className="app-logo" src={logo} alt="logo"/>
-					<Hello tech={this.state.tech}/>
+					<Hello tech={store.getState().tech}/>
 					<a className="app-tech-link"
 					   href="https://redux.js.org"
 					   target="_blank"
